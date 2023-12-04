2228ea177d1c40eafc265113eeea73ddc530dbd6
Crivetkaaa <crivetkaa@vk.com>
2023-12-04 12:51:35 +0300

    The end

--- classfile/DataBase.py
+++ classfile/DataBase.py
@@ -6,33 +6,133 @@ class DataBase:
     cur = conn.cursor()
 
     @classmethod
-    def insert_user(cls, *arg):
-        cls.cur.execute(f"INSERT INTO users_name (user_name) VALUES('{arg[0]}')")
-        cls.conn.commit()
-        cls.cur.execute("SELECT id FROM users_name ORDER BY id DESC")
-        user_name_id = cls.cur.fetchall()[0][0]
+    def insert_user(cls, arg: dict):
+        arg[0] = arg[0][0:30]
+        if arg[0] != '':
+            cls.cur.execute(f"INSERT INTO users_name (user_name) VALUES('{arg[0]}')  ON CONFLICT (user_name) DO NOTHING")
+            cls.conn.commit()
+            cls.cur.execute("SELECT id FROM users_name ORDER BY id DESC")
+            user_name_id = cls.cur.fetchall()[0][0]
+        
+        if arg[1] != '':
+            arg[1] = arg[1][0:30]
+            cls.cur.execute(f"INSERT INTO users_surname (user_surname) VALUES('{arg[1]}')  ON CONFLICT (user_surname) DO NOTHING")
+            cls.conn.commit()
+            cls.cur.execute("SELECT id FROM users_surname ORDER BY id DESC")
+            user_surname_id = cls.cur.fetchall()[0][0]
 
-        cls.cur.execute(f"INSERT INTO users_surname (user_surname) VALUES('{arg[1]}')")
-        cls.conn.commit()
-        cls.cur.execute("SELECT id FROM users_surname ORDER BY id DESC")
-        user_surname_id = cls.cur.fetchall()[0][0]
+        if arg[2] != '':
+            arg[2] = arg[2][0:30]
+            cls.cur.execute(f"INSERT INTO users_lastname (user_lastname) VALUES('{arg[2]}') ON CONFLICT (user_lastname) DO NOTHING")
+            cls.conn.commit()
+            cls.cur.execute("SELECT id FROM users_lastname ORDER BY id DESC")
+            user_lastname_id = cls.cur.fetchall()[0][0]
 
-        cls.cur.execute(f"INSERT INTO users_lastname (user_lastname) VALUES('{arg[2]}')")
-        cls.conn.commit()
-        cls.cur.execute("SELECT id FROM users_lastname ORDER BY id DESC")
-        user_lastname_id = cls.cur.fetchall()[0][0]
-
-        cls.cur.execute(f"INSERT INTO users_phone (user_phone) VALUES('{arg[3]}')")
-        cls.conn.commit()
-        cls.cur.execute("SELECT id FROM users_phone ORDER BY id DESC")
-        user_phone_id = cls.cur.fetchall()[0][0]
-
-        cls.cur.execute(f"INSERT INTO users_address (user_address) VALUES('{arg[4]}')")
-        cls.conn.commit()
-        cls.cur.execute("SELECT id FROM users_address ORDER BY id DESC")
-        user_address_id = cls.cur.fetchall()[0][0]
+        if arg[3] != '':
+            arg[3] = arg[3][0:30]
+            cls.cur.execute(f"INSERT INTO users_phone (user_phone) VALUES('{arg[3]}') ON CONFLICT (user_phone) DO NOTHING")
+            cls.conn.commit()
+            cls.cur.execute("SELECT id FROM users_phone ORDER BY id DESC")
+            user_phone_id = cls.cur.fetchall()[0][0]
+        
+        if arg[4] != '':
+            arg[4] = arg[4][0:30]
+            cls.cur.execute(f"INSERT INTO users_address (user_address) VALUES('{arg[4]}') ON CONFLICT (user_address) DO NOTHING")
+            cls.conn.commit()
+            cls.cur.execute("SELECT id FROM users_address ORDER BY id DESC")
+            user_address_id = cls.cur.fetchall()[0][0]
 
         cls.cur.execute(f"INSERT INTO full_info_user (user_name, lastname, surname, address, phone) VALUES({user_name_id}, {user_lastname_id}, {user_surname_id}, {user_address_id}, {user_phone_id})")
         cls.conn.commit()
         
+    @classmethod
+    def get_all_from_table(cls, table_name:str, column_name:str) ->dict:
+        cls.cur.execute(f"SELECT {column_name} FROM {table_name}")
+        datas = cls.cur.fetchall()
         
+        return_dict = ['']
+        
+        for data in datas:
+            return_dict.append(data[0])
+        
+        return return_dict
+
+    @classmethod
+    def get_users(cls):
+        cls.cur.execute("""SELECT 
+    
+    users_name.user_name,
+    users_surname.user_surname,
+    users_lastname.user_lastname,
+	users_phone.user_phone,
+    users_address.user_address
+    
+FROM 
+    full_info_user
+JOIN 
+    users_name ON full_info_user.user_name = users_name.id
+JOIN 
+    users_surname ON full_info_user.surname = users_surname.id
+JOIN 
+    users_lastname ON full_info_user.lastname = users_lastname.id
+JOIN 
+    users_phone ON full_info_user.phone = users_phone.id
+JOIN 
+    users_address ON full_info_user.address = users_address.id;
+""")
+        return cls.cur.fetchall()
+
+    @classmethod
+    def search(cls, *args):
+        users_info = []
+        for arg in args:
+            if arg == '':
+                arg = '*'
+                users_info.append(arg)
+            else:
+                users_info.append(arg)
+
+        sql_command = '''SELECT 
+    
+    users_name.user_name,
+    users_surname.user_surname,
+    users_lastname.user_lastname,
+	users_phone.user_phone,
+    users_address.user_address
+    
+FROM 
+    full_info_user
+JOIN 
+    users_name ON full_info_user.user_name = users_name.id
+JOIN 
+    users_surname ON full_info_user.surname = users_surname.id
+JOIN 
+    users_lastname ON full_info_user.lastname = users_lastname.id
+JOIN 
+    users_phone ON full_info_user.phone = users_phone.id
+JOIN 
+    users_address ON full_info_user.address = users_address.id
+	
+WHERE '''
+        data = [
+            'users_name.user_name =',
+            'users_surname.user_surname =',
+            'users_lastname.user_lastname =',
+            'users_phone.user_phone =',
+            'users_address.user_address ='
+        ]
+        count = 0
+        for i in range(0, len(users_info)):
+            if users_info[i] != '*':
+                if count > 0:
+                    sql_command += f"AND {data[i]} '{users_info[i]}'"
+                else:
+                    sql_command += f"{data[i]} '{users_info[i]}'"
+                    count += 1
+
+
+        if count > 0:
+            cls.cur.execute(sql_command)
+            return cls.cur.fetchall()
+        else:
+            return cls.get_users()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from mainwindow import *
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QMessageBox, QTableWidgetItem
 import sys
 from classfile.DataBase import DataBase as db
 
@@ -12,7 +12,57 @@ class Interface(QtWidgets.QWidget):
         self.ui.setupUi(self)
         self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
+        self.fill_combobox()
+        self.update_table(users = db.get_users())
         self.ui.pushButton.clicked.connect(self.add_user)
+        self.ui.pushButton_2.clicked.connect(self.search_user)
+
+    def update_table(self, users):
+        rows = len(users)
+        self.ui.tableWidget.setRowCount(rows)
+
+        row = 0
+        for user in users:
+            self.ui.tableWidget.setItem(row, 0, QTableWidgetItem((user[0])))
+            self.ui.tableWidget.setItem(row, 1, QTableWidgetItem((user[1])))
+            self.ui.tableWidget.setItem(row, 2, QTableWidgetItem((user[2])))
+            self.ui.tableWidget.setItem(row, 3, QTableWidgetItem((user[3])))
+            self.ui.tableWidget.setItem(row, 4, QTableWidgetItem((user[4])))
+
+            row += 1
+
+    def clear_combobox(self):
+        self.ui.comboBox.clear()
+        self.ui.comboBox_2.clear()        
+        self.ui.comboBox_3.clear()        
+        self.ui.comboBox_4.clear()        
+        self.ui.comboBox_5.clear()        
+
+    def fill_combobox(self):
+        user_name = DataBase.get_all_from_table(table_name='users_name', column_name='user_name')
+        self.ui.comboBox.addItems(user_name)
+
+        user_surname = DataBase.get_all_from_table(table_name='users_surname', column_name='user_surname')
+        self.ui.comboBox_2.addItems(user_surname)
+
+        user_lastname = DataBase.get_all_from_table(table_name='users_lastname', column_name='user_lastname')
+        self.ui.comboBox_3.addItems(user_lastname)
+
+        user_phone = DataBase.get_all_from_table(table_name='users_phone', column_name='user_phone')
+        self.ui.comboBox_4.addItems(user_phone)
+
+        user_address = DataBase.get_all_from_table(table_name='users_address', column_name='user_address')
+        self.ui.comboBox_5.addItems(user_address)
+
+    def search_user(self):
+        user_name = self.ui.comboBox.currentText()
+        user_surname = self.ui.comboBox_2.currentText()
+        user_lastname = self.ui.comboBox_3.currentText()
+        user_phone = self.ui.comboBox_4.currentText()
+        user_address = self.ui.comboBox_5.currentText()
+
+        users = db.search(user_name, user_surname, user_lastname, user_phone, user_address)
+        self.update_table(users=users)
 
     def add_user(self):
         user_name = self.ui.lineEdit.text()
@@ -20,8 +70,24 @@ class Interface(QtWidgets.QWidget):
         user_lastname = self.ui.lineEdit_3.text()
         user_phone = self.ui.lineEdit_4.text()
         user_address = self.ui.lineEdit_5.text()
-        db.insert_user(user_name, user_surname, user_lastname, user_phone, user_address)
+        
+        data_dict = self.refresh_data(user_name, user_surname, user_lastname, user_phone, user_address)
+        db.insert_user(data_dict)
+        self.update_table(users = db.get_users())
+        self.clear_combobox()
+        self.fill_combobox()
 
+    def save_data(self):
+        pass
+        
+
+    def refresh_data(self, *args) -> dict:
+        user_name = args[0][0:30] if len(args[0]) > 30 else args[0]
+        user_surname = args[1][0:30] if len(args[1]) > 30 else args[1]
+        user_lastname = args[2][0:30] if len(args[2]) > 30 else args[2]
+        user_phone = args[3][0:11] if len(args[3]) > 11 else args[3]
+        user_address = args[4][0:20] if len(args[4]) > 20 else args[4]
+        return [user_name, user_surname, user_lastname, user_phone, user_address]
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
